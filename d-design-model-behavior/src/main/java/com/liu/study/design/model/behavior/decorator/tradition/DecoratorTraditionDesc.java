3458242cdbef886de05fc2c3b44369d942541d90
aslangas <1474673858@qq.com>
2020-10-11 17:04:03 +0800

    装饰器模式

---
+++ d-design-model-behavior/src/main/java/com/liu/study/design/model/behavior/decorator/tradition/DecoratorTraditionDesc.java
@@ -0,0 +1,17 @@
+package com.liu.study.design.model.behavior.decorator.tradition;
+
+/**
+ * 装饰模式。
+ *
+ * @desc 
+ * @author Liuweian
+ * @createTime 2020/9/5 21:53
+ * @version 1.0.0
+ */
+public class DecoratorTraditionDesc {
+
+    /**
+     * 装饰模式可以在不创造更多子类的情况下，将<strong>对象</strong>的功能进行扩展。
+     */
+
+}
